a38924b52eeb1057fc0041d2e254a5d09a3734b0
Jacob Barta <jacob.barta25@gmail.com>
2023-06-05 16:19:33 -0400

    mess with thing and stuff

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use actix_web::{App, HttpServer, web, Result, main};
+use actix_web::{App, HttpServer, web, Result, main, http};
 use actix_web::web::Path;
+use actix_cors::Cors;
 use tokio::fs::read_to_string;
 
 #[main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+
+        let cors = Cors::default()
+            .allow_any_origin() // <--- this
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+
         App::new()
+            .wrap(cors)
             .service(event_names)
             .service(event)
             .service(teams)
+            .service(game_configs)
     })
         .bind(("0.0.0.0", 8080))?
         .run()
@@ -20,7 +31,7 @@ async fn get_default(def: &str) -> Result<String> {
 }
 
 fn minimize(s: String) -> String {
-    s.replace(" ", "").replace("\t", "").replace("\n", "")
+    s.replace("", "").replace("\t", "").replace("\n", "")
 }
 
 #[actix_web::get("/event/{event}")]
@@ -36,4 +47,9 @@ async fn event_names() -> Result<String> {
 #[actix_web::get("/teams/{team}")]
 async fn teams(path: Path<(u16)>) -> Result<String> {
     get_default("teams").await
+}
+
+#[actix_web::get("/game-configs")]
+async fn game_configs() -> Result<String> {
+    get_default("game-configs").await
 }
