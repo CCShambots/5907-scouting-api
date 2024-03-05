0a779ad7157ee7ce0e3d8d5388b57ddfa17adb51
aiden <aiden@voth.name>
2024-03-05 14:04:47 -0500

    schedules

--- src/schedules.rs
+++ src/schedules.rs
@@ -7,13 +7,14 @@ use axum::response::{IntoResponse, Response};
 use axum::{Extension, Json};
 use std::sync::Arc;
 use tracing::instrument;
+use crate::transactions::DataType;
 
 #[instrument(skip(schedule, storage_manager))]
 pub async fn add_schedule(
     storage_manager: Extension<Arc<StorageManager>>,
     Json(schedule): Json<Schedule>,
 ) -> SchedulesResponse {
-    match storage_manager.schedules_add(schedule).await {
+    match storage_manager.storable_add(schedule).await {
         Ok(_) => SchedulesResponse::OK,
         Err(_) => SchedulesResponse::FailedToAdd,
     }
@@ -24,8 +25,8 @@ pub async fn get_schedule(
     Path(name): Path<String>,
     storage_manager: Extension<Arc<StorageManager>>,
 ) -> SchedulesResponse {
-    match storage_manager.schedules_get(name).await {
-        Ok(t) => SchedulesResponse::Schedule(t),
+    match storage_manager.storable_get_serialized(name, DataType::Schedule).await {
+        Ok(t) => SchedulesResponse::SerializedSchedule(t),
         Err(_) => SchedulesResponse::FailedToRead,
     }
 }
@@ -35,7 +36,7 @@ pub async fn edit_schedule(
     storage_manager: Extension<Arc<StorageManager>>,
     Json(schedule): Json<Schedule>,
 ) -> SchedulesResponse {
-    match storage_manager.schedules_edit(schedule).await {
+    match storage_manager.storable_edit(schedule).await {
         Ok(_) => SchedulesResponse::OK,
         Err(_) => SchedulesResponse::FailedToEdit,
     }
@@ -43,7 +44,7 @@ pub async fn edit_schedule(
 
 #[instrument(skip(storage_manager))]
 pub async fn list_schedules(storage_manager: Extension<Arc<StorageManager>>) -> SchedulesResponse {
-    match storage_manager.schedules_list().await {
+    match storage_manager.storable_list(DataType::Schedule).await {
         Ok(l) => SchedulesResponse::List(l),
         Err(_) => SchedulesResponse::FailedToRead,
     }
@@ -54,7 +55,7 @@ pub async fn delete_schedule(
     Path(name): Path<String>,
     storage_manager: Extension<Arc<StorageManager>>,
 ) -> SchedulesResponse {
-    match storage_manager.schedules_delete(name).await {
+    match storage_manager.storable_delete(&name, DataType::Schedule).await {
         Ok(_) => SchedulesResponse::OK,
         Err(_) => SchedulesResponse::FailedToDelete,
     }
@@ -63,7 +64,7 @@ pub async fn delete_schedule(
 #[derive(Debug)]
 pub enum SchedulesResponse {
     OK,
-    Schedule(Schedule),
+    SerializedSchedule(Vec<u8>),
     List(Vec<String>),
     FailedToAdd,
     FailedToEdit,
@@ -75,7 +76,7 @@ impl IntoResponse for SchedulesResponse {
     fn into_response(self) -> Response {
         match self {
             SchedulesResponse::OK => StatusCode::OK.into_response(),
-            SchedulesResponse::Schedule(t) => (StatusCode::OK, Json(t)).into_response(),
+            SchedulesResponse::SerializedSchedule(t) => (StatusCode::OK, String::from_utf8(t).unwrap()).into_response(),
             SchedulesResponse::FailedToAdd => StatusCode::BAD_REQUEST.into_response(),
             SchedulesResponse::FailedToEdit => StatusCode::BAD_REQUEST.into_response(),
             SchedulesResponse::FailedToDelete => StatusCode::BAD_REQUEST.into_response(),
