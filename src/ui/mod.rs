751aecbcc5cd610b17572877283fd17a0ecdf216
Breadspeed1 <53412269+Breadspeed1@users.noreply.github.com>
2024-03-12 22:48:44 -0400

    mwah

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -2,16 +2,16 @@ use askama::Template;
 use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse};
 use chrono::Utc;
-use rand::{random, Rng};
 
 const MAIN_PAGE: &str = include_str!("../../static/htmx/index.html");
 
-#[derive(Template)]
+#[derive(Template, Clone)]
 #[template(path = "transaction-history.html")]
 struct TransactionHistoryTemplate {
     transactions: Vec<TransactionTemplate>,
 }
 
+#[derive(Clone)]
 struct TransactionTemplate {
     alt_key: String,
     timestamp: i64,
@@ -20,13 +20,7 @@ struct TransactionTemplate {
 
 pub async fn transaction_history() -> impl IntoResponse {
     let history = TransactionHistoryTemplate {
-        transactions: vec![
-            TransactionTemplate {
-                alt_key: "asdfasdf".into(),
-                timestamp: Utc::now().timestamp_micros(),
-                action: "delete".into(),
-            }
-        ]
+        transactions: vec![TransactionTemplate { alt_key: "asdfasdf".into(), timestamp: Utc::now().timestamp_micros(), action: "delete".into() }; 25]
     };
 
     (StatusCode::OK, Html(history.render().unwrap()))
